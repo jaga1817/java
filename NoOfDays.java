bb5adc538e308c69a0377bedc630bb8b496627da
jaga1817 <bunuynani@gmail.com>
2025-11-18 14:01:41 +0530

    Update NoOfDays.java

--- NoOfDays.java
+++ NoOfDays.java
@@ -7,7 +7,7 @@ class Practice{
         System.out.println("Enter the month you want");
         int month=sc.nextInt();
         int Noofdays=0;
-        if(month>12){
+        if(month<=1||month>12){
             System.out.println("invalid month");
         }
 
@@ -21,10 +21,9 @@ class Practice{
         }
         }else if(month==1||month==3||month==5||month==7||month==8||month==10||month==12){
             Noofdays=31;
-        }else if(month==4||month==6||month==9||month==11){
+        }else {
             Noofdays=30;
-        }else if(month==2){
-            Noofdays=28;
+        
         }
         System.out.println("_________________________");
 
@@ -35,4 +34,5 @@ class Practice{
             
 
     }
+
 }
