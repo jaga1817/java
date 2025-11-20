affbc25352bcadce3f3e5f4e1a7ff9ce7d92fb58
jaga1817 <bunuynani@gmail.com>
2025-11-20 16:05:14 +0530

    Add files via upload

---
+++ PerfectNum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class PerfectNum{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number to check if it's perfect: ");
+        int num=sc.nextInt();
+        int sum=0;
+
+        for (int i=1;i<=num/2;i++){
+            if (num%i==0){
+                sum+=i;
+
+            }
+        }
+        if (sum==num){
+            System.out.println(num+" is a perfect number.");
+        } else {
+            System.out.println(num+" is not a perfect number.");
+        }
+    }
+}
